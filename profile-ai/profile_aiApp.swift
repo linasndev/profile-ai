7e7215ff245c060976886c11756b8a716f4e69c8
Linas Nutautas <linas404@protonmail.com>
2025-02-01 17:57:26 +0100

    Initial Commit

---
+++ profile-ai/profile_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_aiApp.swift
+//  profile-ai
+//
+//  Created by linasdev on 01/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct profile_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
